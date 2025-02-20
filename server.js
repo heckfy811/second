d21f80747146e88b86675e19f433a03fe92aed3e
heckfy811 <murtazin0811@gmail.com>
2025-02-20 17:07:28 +0300

    hotfix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const server = http.createServer((req, res) => {
     });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>404 - Страница не найдена</h1>');
+    res.end('<h1>404 - Page Not Found</h1>');
   }
 });
 
