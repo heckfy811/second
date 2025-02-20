6da6988d90e2633aad1769a4b62dad43561a8e67
heckfy811 <murtazin0811@gmail.com>
2025-02-20 17:04:14 +0300

    js

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Ошибка сервера');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else if (req.url === '/styles.css') {
+    fs.readFile(path.join(__dirname, 'styles.css'), (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Ошибка сервера');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>404 - Страница не найдена</h1>');
+  }
+});
+
+server.listen(8000, () => {
+  console.log('Сервер запущен на http://localhost:8000');
+});
